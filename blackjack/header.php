db4b3c626cb11e18bc5c651b76108ce3e961bb89
brenninger <brenninger01@gmail.com>
2014-04-28 21:35:11 -0500

    Login form Validation

--- blackjack/header.php
+++ blackjack/header.php
@@ -35,6 +35,7 @@ Description: A two-column, fixed-width and lightweight template ideal for 1024x7
 <script src="../js/amcharts/amcharts.js" type="text/javascript"></script>
 <script src="../js/amcharts/serial.js" type="text/javascript"></script>
 <script src="../js/amcharts/themes/chalk.js" type="text/javascript"></script>
+<script src="formVal.js" type="text/javascript"></script>
 </head>
 <body>
 
@@ -59,6 +60,7 @@ Description: A two-column, fixed-width and lightweight template ideal for 1024x7
                         if(isset($_SESSION['admin']))
                             if($_SESSION['admin'] == 1)
                             echo "<li><a href='admin.php'>Admin</a></li>";
+                            
                         ?>
 			<li><a href="#"></a></li>
 			<li><a href="#"></a></li>
